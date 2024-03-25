f6c31041d94031e33b95755aa9743431b1617626
Iryste <jean-baptiste.hamel@hotmail.fr>
2024-03-25 10:58:51 +0100

    html_css

--- app/index.php
+++ app/index.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <!-- link.css pour appeler le fichier CSS -->
     <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
+
 <body>
-    <h1>TEST LAMP</h1>
+<!-- Contenu visible -->
+    <header>
+        <nav>
+            <div>Bonjurno</div>
+            navigation
+        </nav>
+    </header>
+
+<main>
+    <section class="section" id="section">section 1</section>
+    <section class="section">section 2</section>
+    <section class="section">section 3</section>
+</main>
+
+
+
 </body>
 </html>
